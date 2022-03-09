22c9df9cf68820332e6221c7e0296ce00429f9ce
vivekduttamishra <vivek@conceptarchitect.in>
2022-03-09 17:42:50 +0530

    day#3 update#2

---
+++ basic-html-css-js/basic-js/demo07-assignment.js
@@ -0,0 +1,15 @@
+
+var U = [2, 12, "hello", new Date(), 17, 21, "world"];
+
+
+//write a program to separate the list into two lists numbers and others.
+
+
+
+//display the two lists
+
+console.log('others',others);
+console.log('numbers',numbers);
+
+
+
