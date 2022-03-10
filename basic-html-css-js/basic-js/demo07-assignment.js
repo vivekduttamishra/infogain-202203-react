bff5403cd9687edfab318155963856ad4f5dddb0
vivekduttamishra <vivek@conceptarchitect.in>
2022-03-10 15:56:51 +0530

    day#4 update#1

--- basic-html-css-js/basic-js/demo07-assignment.js
+++ basic-html-css-js/basic-js/demo07-assignment.js
@@ -1,9 +1,18 @@
 
-var U = [2, 12, "hello", new Date(), 17, 21, "world"];
+var U = [2, 12, "hello", -1,  new Date(), 17, 21, "world"];
 
 
 //write a program to separate the list into two lists numbers and others.
+var numbers=[];
+var others=[];
 
+for(var value of U)
+{
+    if(typeof(value)==='number')  
+        numbers.push(value);
+    else
+        others.push(value);
+}
 
 
 //display the two lists
---
+++ basic-html-css-js/basic-js/demo08-function.js
@@ -0,0 +1,11 @@
+
+
+function greet(name){
+    
+    return 'Hello '+name+'! welcome to Java Script';
+}
+
+
+console.log(greet('World'));
+
+console.log(greet('Team'));
