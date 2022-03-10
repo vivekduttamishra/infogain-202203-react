152c34111f3bf0a7b8c21d27cf487e45796435ec
Patan Liyakath Alikhan <Patan.Alikhan@igglobal.com>
2022-03-10 10:12:04 +0530

    message

---
+++ demo07-assignment.js
@@ -0,0 +1,14 @@
+var U = [2, 12, "hello", new Date(), 17, 21, "world"];
+var others = ["hello","world",new Date()]
+others.length;
+var arr = [2,12,17,21];
+var len= arr.length;
+console.log(len);
+console.log(arr.length);
+console.log('others',others);
+console.log('numbers',numbers);
+
+
+
+
+
