507a83c72dff9d8ed68d89716f4292fba40c6b0d
Vella Boina Radha <VellaBoina.Radha@igglobal.com>
2022-03-14 19:27:54 +0530

    assignment5

--- Assignments/demo07-assignment.js
+++ Assignments/demo07-assignment.js
@@ -1,19 +1,40 @@
 
-var U = [2, 12, "hello", new Date(), 17, 21, "world"];
-var numbers= [];
-var others=[];
-U.filter((n)=>{
-    if(typeof (n) == "number")
-    numbers.push(n)
-    else
-    others.push(n)
-})
-//write a program to separate the list into two lists numbers and others.
 
-//display the two lists
+// var U = [2, 12, "hello", new Date(), 17, 21, "world"];
+// var numbers= [];
+// var others=[];
+// U.filter((n)=>{
+//     if(typeof (n) == "number")
+//     numbers.push(n)
+//     else
+//     others.push(n)
+// })
+// //write a program to separate the list into two lists numbers and others.
 
-console.log('numbers',numbers);
-console.log('others',others);
+// //display the two lists
+
+// console.log('numbers',numbers);
+// console.log('others',others);
+
+
+function average() {
+    var length = arguments.length;
+    var sum = 0;
+    
+    for(var i=0; i<length; i++) {
+        if (!isNaN(arguments[i])) {
+            sum = sum + arguments[i];
+        }
+    }
+    return sum/length;
+  }
+  
+  console.log(average(2,3));
+  console.log(average(2,3,9,8,11,4,2))
+  console.log(average(1,2,3,"hello",4,5))
+
+
+
 
 
 
