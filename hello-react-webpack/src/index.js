b6aeb948f9a3b99ba19fc1ae232b7a168c701847
purva tayde <purvatayde1908@gmail.com>
2022-03-23 14:29:06 +0530

    message

---
+++ hello-react-webpack/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+
+const component=React.createElement("h1",null,"Welcome React Webpack");
+ReactDOM.render(component,document.getElementById("placeholder"));
+
