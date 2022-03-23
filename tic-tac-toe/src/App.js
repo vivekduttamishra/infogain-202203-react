fe840fd779a3482809b18735bd24d0028ffb172f
vivekduttamishra <vivek@conceptarchitect.in>
2022-03-23 18:14:26 +0530

    day 11 update 2

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import Game from './components/Game';
-
+import Clock from './components/Clock';
 const App =  ()=>{
 
     return (
         <div className="app">
             <Header/>
+            <Clock/>
             <Game/>
         </div>
     );
--- tic-tac-toe/src/components/Board.js
+++ tic-tac-toe/src/components/Board.js
@@ -8,17 +8,10 @@ const Board=(props)=>{
         return (
             <div className='board'>
     
-                <Cell value={props.cells[0]} id={0} onCellClick={props.onCellClick}/>
-                <Cell value={props.cells[1]} id={1} onCellClick={props.onCellClick}/>
-                <Cell value={props.cells[2]} id={2} onCellClick={props.onCellClick}/>
-                
-                <Cell value={props.cells[3]} id={3} onCellClick={props.onCellClick}/>
-                <Cell value={props.cells[4]} id={4} onCellClick={props.onCellClick}/>
-                <Cell value={props.cells[5]} id={5} onCellClick={props.onCellClick}/>
-                
-                <Cell value={props.cells[6]} id={6} onCellClick={props.onCellClick}/>
-                <Cell value={props.cells[7]} id={7} onCellClick={props.onCellClick}/>
-                <Cell value={props.cells[8]} id={8} onCellClick={props.onCellClick}/>
+                {props.cells.map((cell,index)=>( <Cell value={cell} key={index} id={index} onCellClick={props.onCellClick}/>))}
+
+               
+               
                 
                 
             </div>
