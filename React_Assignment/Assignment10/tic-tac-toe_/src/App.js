4a19bbfb7c049013302d4f3f7f037eb79cf2da12
Kumari Reena <Kumari.Reena@igglobal.com>
2022-03-24 13:15:10 +0530

    message

---
+++ React_Assignment/Assignment10/tic-tac-toe_/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Game from './components/Game';
+
+const App =  ()=>{
+
+    return (
+        <div className="app">
+            <Header/>
+            <Game/>
+        </div>
+    );
+};
+
+export default App;
+
