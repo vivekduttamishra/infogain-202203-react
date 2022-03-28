5fab1806c6c459a354849870a8d35ee393338457
Abhishek Mahajan <Abhishek.Mahajan@igglobal.com>
2022-03-28 17:28:24 +0530

    message

---
+++ books-web-v1/src/screens/BookAddScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import withTitle from '../utils/withTitle';
+
+const BookAddScreen=({})=>{
+    //TODO: Initialize Here
+    
+
+    return (
+        <div className='BookAddScreen'>
+            
+        </div>
+    );
+}
+
+export default withTitle( BookAddScreen, "Add New Book");
